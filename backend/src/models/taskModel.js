8f5dfcf141629c855521cf3c05b352a00bbb4127
pablorenan-dev <pablonichterwistz@outlook.com>
2026-04-19 14:19:25 -0300

    criado o básico do frontEnd, com o básico do backend, com as rotas mapeadas

---
+++ backend/src/models/taskModel.js
@@ -0,0 +1,30 @@
+import pool from '../config/db.js';
+
+// listar todas as tarefas
+export const getAllTaks = async => {
+    const result = await pool.query('SELECT * FROM tasks');
+    return result.rows;
+};
+
+// criar uma task no banco, com o tiutlo e descricao
+export const createTask = async (title, description) => {
+    const result = await pool.query(
+        'INSERT INTO tasks (title, description) VALUES ($1, $2) RETURNING *',
+        [title, description]
+    );
+    return result.rows[0];
+};
+
+// atualiza se a task esta completa pelo id
+export const updateTask = async (id) => {
+    const result = await pool.query(
+        'UPDATE tasks SET completed = NOT completed WHERE id = $1 RETURNING *',
+        [id]
+    );
+    return result.rows[0];
+};
+
+// deleta a task pelo id
+export const deleteTask = async (id) => {
+    await pool.query('DELETE FROM tasks WHERE id = 1$', [id]);
+};

3cbe652cbd2acc25017a10804e0da7ea9f820daf
pablorenan-dev <pablonichterwistz@outlook.com>
2026-04-19 20:31:17 -0300

    Ajustado o front para ficar mais visualmente agradavel

--- taskManager/src/components/taskHeader/TaskHeader.jsx
+++ taskManager/src/components/taskHeader/TaskHeader.jsx
@@ -1,24 +1,31 @@
 import "./TaskHeader.css"
 import { useState } from "react";
+import logo from "../../assets/sfx-logo.png";
 
 function TaskHeader({createTask}){
     const [title, setTitle] = useState("");
     const [description, setDescription] = useState("");
 
     return (
-        <div className="header">
-            <div>
-                <input type="text" placeholder="Title..." onChange={(e) => setTitle(e.target.value)}> 
-
-                </input>
-                <input type="text" placeholder="Description..." onChange={(e) => setDescription(e.target.value)}>
-
-                </input>
+        <div>
+            <div className="header">
+                <img className="sfx-logo" src={logo}></img>
+                <h1 className="title">Task Manager</h1>
             </div>
-            
-            <button onClick={() => title ? createTask(title, description) : "Title needs a value"}>
-                Submit 
-            </button>
+            <div className="header">
+                <div>
+                    <input className="input" type="text" placeholder="Title..." onChange={(e) => setTitle(e.target.value)}> 
+
+                    </input>
+                    <input className="input" type="text" placeholder="Description..." onChange={(e) => setDescription(e.target.value)}>
+
+                    </input>
+                </div>
+                
+                <button className="btn-submit" onClick={() => title ? createTask(title, description) : "Title needs a value"}>
+                    Submit 
+                </button>
+            </div>
         </div>
     )
 }
--- taskManager/src/components/taskList/TaskList.jsx
+++ taskManager/src/components/taskList/TaskList.jsx
@@ -4,20 +4,21 @@ function TaskList({list, changeStatusTask, deleteTask}){
 
     return (
         list.map((task) => (
-        <div className={task.completed ? "task completed" : "task"} key={task.id}>
+        <div className="task-list" key={task.id}>
+            <div className={task.completed ? "task completed" : "task"}>
 
-            <div>
+                <div className="task-text">
 
-                <h1>{task.title}</h1>
-                <p>{task.description}</p>
+                    <h1 className="task-title">• {task.title}</h1>
+                    <p className="task-description">{task.description}</p>
 
-            </div>
-            <div>
-                <button onClick={() => changeStatusTask(task.id)}>🟢</button>
-                <button onClick={() => deleteTask(task.id)}>🔴</button>
+                </div>
+                <div>
+                    <button className="btn-actions" onClick={() => changeStatusTask(task.id)}>✅</button>
+                    <button className="btn-actions" onClick={() => deleteTask(task.id)}>❌</button>
+                </div>
             </div>
         </div>
-
         ))
    
     );

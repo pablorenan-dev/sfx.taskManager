8b18d0872f5429c4131dab8fa94d92704f962a36
pablorenan-dev <pablonichterwistz@outlook.com>
2026-04-19 21:22:48 -0300

    adicionado o sql do banco na root

---
+++ database.sql
@@ -0,0 +1,7 @@
+  CREATE TABLE tasks (
+  id SERIAL PRIMARY KEY,
+  title VARCHAR(255) NOT NULL,
+  description TEXT,
+  completed BOOLEAN NOT NULL DEFAULT false,
+  created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
